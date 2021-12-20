3a3fb848aa9e02825af1bef5788d484d0cd906f7
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-20 15:28:18 -0300

    Criar classe CarroService

---
+++ src/main/java/br/com/zup/ZupCarExe/carro/CarroService.java
@@ -0,0 +1,14 @@
+package br.com.zup.ZupCarExe.carro;
+
+import br.com.zup.ZupCarExe.carro.dtos.CarroDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarroService {
+    private List<CarroDto> concessionaria = new ArrayList<>();
+
+
+}
