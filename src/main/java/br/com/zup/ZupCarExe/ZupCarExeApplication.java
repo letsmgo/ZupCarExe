1e77c2770842632005dc49ac8ab600cee3f82478
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-20 15:21:38 -0300

    first commit

---
+++ src/main/java/br/com/zup/ZupCarExe/ZupCarExeApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupCarExe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupCarExeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupCarExeApplication.class, args);
+	}
+
+}
